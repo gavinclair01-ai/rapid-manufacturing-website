ff4ebe00d16c845e9fbf1abd87c5d36cdc39e63d
Sebastian <sebastian@rapidmanufacturing.com.au>
2026-02-27 11:28:32 +1100

    feat: add Google Analytics 4 (G-KH75NSB4XK)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Barlow_Condensed, Outfit } from "next/font/google";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
+
+const GA_ID = "G-KH75NSB4XK";
 
 const barlowCondensed = Barlow_Condensed({
   variable: "--font-barlow-condensed",
@@ -35,6 +38,18 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}');
+          `}
+        </Script>
       </body>
     </html>
   );
